d2ca7c5706ce4af24c21d902d08e16c5094ebecc
Jdender~ <jdenderplays@gmail.com>
2019-02-19 13:12:43 -0500

    Update client index

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,18 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
+import { client } from './graphql';
+import Buefy from 'buefy';
+
+Vue.use(VueApollo);
+Vue.use(Buefy);
+
+const apolloProvider = new VueApollo({
+    defaultClient: client,
+});
 
 new Vue({
     el: '#app',
     render: h => h(App),
+    apolloProvider,
 });
