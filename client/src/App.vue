028d58e720e388193ea24b884c907049371cc4bd
Jdender~ <jdenderplays@gmail.com>
2019-02-19 13:13:28 -0500

    Split Send and View from App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,59 +1,17 @@
 <template>
-    <div>
-        <input 
-            type="text"
-            @keyup.enter="sendWorkingMessage"
-            v-model="workingMessage"
-        />
-        <ul>
-            <li v-for="(msg, key) of messages" :key="key">{{msg}}</li>
-        </ul>
-    </div>
+    
 </template>
 
+<style>
+body {
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    background-color: rgba(114,118,125,.3);
+}
+</style>
+
 <script lang="ts">
-import Vue from 'vue';
-
+import Vue from 'vue'
 export default Vue.extend({
-
-    data() {
-        return {
-            workingMessage: '',
-            premade: [
-                'Can you turn down the Piano?',
-                'Can you turn up the Piano?',
-                'Can you turn down the Drums?',
-                'Can you turn up the Drums?',
-                'The Bass is too loud.',
-                'The Bass is too quiet.',
-                'The Vocals are too loud.',
-                'The Vocals are too soft.',
-            ],
-            messages: [
-                'test',
-                'one',
-                'two',
-                'three',
-            ]
-        };
-    },
-
-    methods: {
-
-        sendWorkingMessage() {
-
-            this.sendMessage(this.workingMessage);
-
-            this.workingMessage = '';
-        },
-
-        sendPreMessage() {
-
-        },
-
-        sendMessage(content: string) {
-            this.messages.push(content);
-        }
-    },
-});
+    
+})
 </script>
---
+++ client/src/pages/Send.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <input
+            type="button"
+            v-for="(pre, key) of premade"
+            :value="pre"
+            :key="key"
+            @click="sendMessage(key)"
+        >
+        <input 
+            type="text"
+            v-model="workingMessage"
+            @keyup.enter="sendMessage()"
+        />
+    </div>
+</template>
+
+<style scoped>
+input {
+    padding: 1%;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import gql from 'graphql-tag';
+import 'vue-apollo';
+
+export default Vue.extend({
+
+    name: 'Send',
+
+    data() {
+        return {
+            workingMessage: '',
+            premade: [
+                'Can you turn down the Piano?',
+                'Can you turn up the Piano?',
+                'Can you turn down the Drums?',
+                'Can you turn up the Drums?',
+                'The Bass is too loud.',
+                'The Bass is too quiet.',
+                'The Vocals are too loud.',
+                'The Vocals are too soft.',
+            ],
+            messages: [
+                'test',
+                'one',
+                'two',
+                'three',
+            ],
+        };
+    },
+
+    methods: {
+
+        sendMessage(index?: number) {
+
+            const content = index
+            ? this.premade[index]
+            : this.workingMessage;
+
+            if (!content || content.length === 0) return;
+
+            this.$apollo.mutate({
+                mutation: gql`
+                    mutation($data: MessageSendInput!) {
+                        sendMessage(data: $data) {
+                            id
+                            content
+                        }
+                    }
+                `,
+                variables: {
+                    data: {
+                        content,
+                    },
+                },
+            });
+
+            this.workingMessage = '';
+        },
+    },
+});
+</script>
